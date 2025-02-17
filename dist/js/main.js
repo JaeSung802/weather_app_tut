d11dc1c385dbde79ae35d21e00b2273591bd15f7
Henri <hanjs802@gmail.com>
2025-02-17 19:12:29 +0900

    main.js 작업중중

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,10 +4,20 @@ const currentLoc = new CurrentLocation();
 const initApp = () => {
     // add listeners
     const geoButton = document.getElementById("getLocation");
-    geoButton.addEventListener("click", getGeoWeather)
+    geoButton.addEventListener("click", getGeoWeather);
     // set up
 
     // load weather
-}
+};
 
 document.addEventListener("DOMContentLoaded", initApp);
+
+const getGeoWeather = (event) => {
+    if (event) {
+        if (event.type === "click") {
+            // add spinner
+        }
+    }
+    if (!navigator.geolocation) geoError();
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+}
