21a298e81be2346692feebc91b52ea0779c24438
Henri <hanjs802@gmail.com>
2025-02-19 21:54:17 +0900

    update : js

--- dist/js/domFunctions.js
+++ dist/js/domFunctions.js
@@ -9,3 +9,21 @@ const animateButton = (element) => {
     element.nextElementSibling.classList.toggle("block");
     element.nextElementSibling.classList.toggle("none");
 };
+
+// 디스플레이 오류 (헤더 메시지, 화면에서 받는 독자 메시지지)
+export const displayError = (headermsg, srMsg) => {
+    // 날씨 위치 헤더 메시지 업데이트
+    updateWeatherLocationHeader(headermsg);
+    // 화면 판독기 확인 업데이트
+    updateScreenReaderConfirmation(srMsg);
+};
+
+const updateWeatherLocationHeader = (message) => {
+    const h1 = document.getElementById("currentForecat__location");
+    h1.textContent = message;
+};
+
+const updateScreenReaderConfirmation = (message) => {
+    // 어디에 있는지 선택
+    document.getElementById("confirmation").textContent = message;
+};
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,4 @@
-import { addSpinner } from "./domFunctions.js";
+import { addSpinner, displayError } from "./domFunctions.js";
 import CurrentLocation from "./CurrentLocation.js";
 const currentLoc = new CurrentLocation();
 
@@ -24,6 +24,27 @@ const getGeoWeather = (event) => {
         }
     }
     // 네비게이터 지리적 함수
-    // if (!navigator.geolocation) return geoError(); // 실패 : geo 에러 호출
-    // navigator.geolocation.getCurrentPosition(geoSuccess, geoError); // 성공 : 지리적 위치 가져오기
+    if (!navigator.geolocation) return geoError(); // 실패 : geo 에러 호출
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError); // 성공 : 지리적 위치 가져오기
 }
+
+// 오류 함수
+const geoError = (errObj) => {
+    // 오류 메시지지
+    const errMsg = errObj ? errObj.message : "Geolocation not supported";
+    // 디스플레이 오류 기능
+    displayError(errMsg, errMsg);
+
+}
+
+// 위치 함수(위치값 전달)
+const geoSuccess = (position) => {
+    // 좌표 객체에 대한 코드
+    const myCoordsObj = {
+        lat : position.coords.latitude, // 위도
+        lon : position.coords.longitute, // 경도
+        name : `Lat: ${position.coords.latitude} Long:${position.coords.longitute}`
+    };
+    // set location object(위치 설정)
+    // update data and display(데이터 및 디스플레이 업데이트)
+};
